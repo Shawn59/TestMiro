637ca358d9666d3cd978b3591679a845d31f469a
Shawn59 <xxx_respekt.ru@mail.ru>
2020-04-11 19:29:03 +0500

    #master - add main functionality

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-import EmailsInput from './components/emails-input.js';
+import {EmailsInput} from './components/emails-input.js';
 let inputContainerNode = document.querySelector('#emails-input');
-let emailsInput = new EmailsInput(inputContainerNode, {});
+let options = {
+    emailList: [
+        "sdfds@dfd.df",
+        "dsfsdfds@dfd.gf",
+        "sdfsdf@dfdf",
+        "fdsgfdlfghfg@dfds.df"
+    ]
+};
+let btn1 = document.getElementById('addEmail');
+let btn2 = document.getElementById('showCountEmail');
+let emailsInput = new EmailsInput(inputContainerNode, options);
+
+const getCountEmails = () => {
+    console.log(emailsInput.getEmails().length);
+};
+
+const addRandomEmail = () => {
+    let email = Math.random().toString(36).substr(4, 10) + '@gmail.com';
+    emailsInput.addChip(email);
+};
+
+btn1.onclick = addRandomEmail;
+btn2.onclick = getCountEmails;
--- components/emails-input.js
+++ components/emails-input.js
@@ -1,36 +1,100 @@
-export default class EmailsInput {
+const emailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+class EmailsInput {
     doc = document;
     headElem = '';
     blockEmail = ''; // сюда емейлы добавляем
     input = '';
+    emailList = [];
+
     constructor(parentNode, options) {
         this.init(parentNode, options);
     }
 
-    init = (parentNode, options) => {
-        this.headElem = this.doc.createElement('div');
+    init = (parentNode, options = {}) => {
+        //клонируем
+        this.headElem = this.doc.createElement("div");
         this.headElem.classList.add("insert-email");
-        this.headElem.innerHTML = "<div>" +
-            "<span> Share <strong>Board name</strong> with others</span>" +
-                "<div class='error-block'>" +
-                    "<div class='insert-block'>" +
-                        "<input type='text' class='input-field' placeholder='add more emails...'>" +
-                    "</div>" +
+        this.headElem.innerHTML =
+            "<div class='error-block'>" +
+                "<div class='insert-block'>" +
+                    "<input type='text' class='input-field' placeholder='add more emails...'>" +
                 "</div>" +
             "</div>";
         parentNode.appendChild(this.headElem);
         this.blockEmail = this.headElem.getElementsByClassName("insert-block")[0];
         this.input = this.blockEmail.getElementsByClassName("input-field")[0];
-        this.input.onkeyup = this.addChip;
-        console.log(this.input);
+        this.input.onkeyup = this.handleInputKeyUp;
+        this.input.onblur = this.handleInputBlur;
+
+        this.setChipList(options.emailList);
     };
 
-    addChip = (event) => {
-        if(event.key === 'Enter'){
-            let el = this.doc.createElement("div");
-            el.textContent = event.currentTarget.value;
-            this.blockEmail.insertBefore(el, this.input);
+    setChipList = (sourceEmailList) => {
+        sourceEmailList.forEach(item => {
+            this.addChip(item)
+        });
+    };
+
+    handleInputKeyUp = (event) => {
+        let email = event.currentTarget.value;
+        // чтобы нельзя было добавлять пустой чип
+        // даём
+        if (event.key === ",") {
+            email = email.replace(',', '');
         }
-    }
+        if (email.trim() !== "" && (event.key === "Enter" || event.key === ",")) {
+            this.addChip(email);
+            //чистим поле
+            event.currentTarget.value = "";
+        }
+    };
+
+    handleInputBlur = (event) => {
+        if (event.currentTarget.value.trim() !== "") {
+            this.addChip(event.currentTarget.value);
+            //чистим поле
+            event.currentTarget.value = "";
+        }
+    };
+
+    addChip = (email) => {
+        let isValid = emailPattern.test(email);
+        let chipDivEl = this.doc.createElement("div");
+        let spanEl = this.doc.createElement("span");
+        let imgEl = this.doc.createElement("img");
+
+        chipDivEl.classList.add(isValid ? "chip" : "chip-invalid");
+        spanEl.textContent = email;
+        imgEl.src = "./components/images/remove.svg";
+        imgEl.classList.add("closed-img");
+        imgEl.onclick = () => this.deleteChip(chipDivEl, email);
+
+        chipDivEl.appendChild(spanEl);
+        chipDivEl.appendChild(imgEl);
+
+        this.blockEmail.insertBefore(chipDivEl, this.input);
+
+        this.emailList.push(email);
+    };
+
+    deleteChip = (chipElem, email) => {
+        chipElem.parentNode.removeChild(chipElem);
+        let indexEmail = this.emailList.indexOf(email);
+        if (indexEmail !== -1) {
+            this.emailList.splice(indexEmail, 1);
+        }
+    };
+
+    getEmails = () => {
+        return this.emailList;
+    };
+
+   /* checkValid = (email) => {
+        return emailPattern.test(email);
+    };*/
+
 };
+
+export {EmailsInput};
 
